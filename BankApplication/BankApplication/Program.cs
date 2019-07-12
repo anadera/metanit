c496ac64b9a99ec4da616eb1d1b95e1d0fe59dc6
Anzhelika Deriabina <hey_biscuit@mail.ru>
2019-07-12 19:08:56 +0300

    add main program + project cfgs

---
+++ BankApplication/BankApplication/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankLibrary;
+
+namespace BankApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank<Account> bank = new Bank<Account>("SEB");
+            bool alive = true;
+            while (alive)
+            {
+                ConsoleColor color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("1. Открыть счет \t 2. Вывести средства \t 3. Добавить на счет");
+                Console.WriteLine("4. Закрыть счет \t 5. Пропустить день \t 6. Выйти из программы");
+                Console.WriteLine("Введите номер пункта:");
+                Console.ForegroundColor = color;
+                try
+                {
+                    int command = Convert.ToInt32(Console.ReadLine());
+
+                    switch (command)
+                    {
+                        case 1:
+                            OpenAccount(bank);
+                            break;
+                        case 2:
+                            Withdraw(bank);
+                            break;
+                        case 3:
+                            Put(bank);
+                            break;
+                        case 4:
+                            CloseAccount(bank);
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            alive = false;
+                            continue;
+                    }
+                    bank.CalculatePercentage();
+                }
+                catch (Exception ex)
+                {
+                    color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = color;
+                }                
+            
+            }
+        }
+
+        private static void OpenAccount(Bank<Account> bank)
+        {
+
+        }
+        private static void Withdraw(Bank<Account> bank)
+        {
+
+        }
+        private static void Put(Bank<Account> bank)
+        {
+
+        }
+        private static void CloseAccount(Bank<Account> bank)
+        {
+
+        }
+    }
+}
