7b7ceb326012d7d86d82ed0d6ba488e977b5f5f1
Anzhelika Deriabina <hey_biscuit@mail.ru>
2019-07-12 18:06:38 +0300

    add AccountStateHandler

--- BankApplication/BankLibrary/AccountStateHandler.cs
+++ BankApplication/BankLibrary/AccountStateHandler.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace BankLibrary
 {
-    class AccountStateHandler
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+    public class AccountEventArgs
     {
+        public string Message { get; private set; }
+
+        public decimal Sum { get; private set; }
+
+        public AccountEventArgs(string _mes, decimal _sum)
+        {
+            Message = _mes;
+            Sum = _sum;
+        }
+        
     }
 }
